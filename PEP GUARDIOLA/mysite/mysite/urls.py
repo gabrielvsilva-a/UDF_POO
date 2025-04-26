a4a73d221ac91647682fee502fbe272aca8add17
gabrielvsilva-a <gbsilvaa.dev@gmail.com>
2025-04-25 21:55:33 -0300

    Delete PEP GUARDIOLA/mysite directory

--- PEP GUARDIOLA/mysite/mysite/urls.py
+++
@@ -1,9 +0,0 @@
-# mysite/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('__debug__/', include('debug_toolbar.urls')),
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-]
